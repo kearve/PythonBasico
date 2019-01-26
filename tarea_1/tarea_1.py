22cea00661beb9e53be4ebcd532df0d2693de7ff
Kenneth Arrieta <kearve@gmail.com>
2019-01-26 12:38:02 -0600

    Primera Tarea

---
+++ tarea_1/tarea_1.py
@@ -0,0 +1,9 @@
+import math
+pi=math.pi
+asterisk="*******"
+print (asterisk[1:2],"%.7f" %math.pi , end='')
+print (" ", asterisk[1:3],"%.6f" %math.pi , end='')
+print (" ", asterisk[1:4],"%.5f" %math.pi , end='')
+print (" ", asterisk[1:5],"%.4f" %math.pi , end='')
+print (" ", asterisk[1:6],"%.3f" %math.pi , end='')
+print (" ", asterisk[1:7],"%.2f" %math.pi)
